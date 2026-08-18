a4ed5d5908c371c3523393efc746b76e1963805d
Anton <anton.volovatov2001@yandex.ru>
2026-08-18 23:06:40 +0300

    исправялю ошибки5

--- main_svc/src/main/java/ru/practicum/main/event/EventController.java
+++ main_svc/src/main/java/ru/practicum/main/event/EventController.java
@@ -4,16 +4,12 @@ package ru.practicum.main.event;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 import ru.practicum.main.event.dto.EventFullDto;
 import ru.practicum.main.event.dto.EventShortDto;
 import ru.practicum.main.event.dto.NewEventDto;
 import ru.practicum.main.event.service.EventService;
-import ru.practicum.main.exception.ForbiddenException;
 import ru.practicum.main.request.dto.ParticipationRequestDto;
-import ru.practicum.main.user.model.User;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
@@ -31,7 +27,7 @@ public class EventController {
     public List<EventShortDto> getEvents(@PathVariable Long userId, @RequestParam(defaultValue = "0") int from,
                                          @RequestParam(defaultValue = "10") int size) {
 
-        authorizationVerification(userId);
+//        authorizationVerification(userId);
 
         return eventService.getEvents(userId, from, size);
     }
@@ -39,7 +35,7 @@ public class EventController {
     @PostMapping("/users/{userId}/events")
     public EventFullDto createEvent(@PathVariable Long userId, @RequestBody NewEventDto newEventDto) {
 
-        authorizationVerification(userId);
+//        authorizationVerification(userId);
 
         return eventService.createEvent(userId, newEventDto);
     }
@@ -47,7 +43,7 @@ public class EventController {
     @GetMapping("/users/{userId}/events/{eventId}")
     public EventFullDto getFullEventById(Long userId, Long eventId) {
 
-        authorizationVerification(userId);
+//        authorizationVerification(userId);
 
         return eventService.getFullEventById(userId, eventId);
     }
@@ -55,7 +51,7 @@ public class EventController {
     @PatchMapping("/users/{userId}/events/{eventId}")
     public EventShortDto updateEvent(@PathVariable Long userId, @PathVariable Long eventId, @RequestBody EventShortDto eventShortDto) {
 
-        authorizationVerification(userId);
+//        authorizationVerification(userId);
 
         return eventService.updateEvent(userId, eventId, eventShortDto);
     }
@@ -63,28 +59,28 @@ public class EventController {
     @GetMapping("/users/{userId}/events/{eventId}/requests")
     public List<ParticipationRequestDto> getEventRequestsForUser(@PathVariable Long userId, @PathVariable Long eventId) {
 
-        authorizationVerification(userId);
+//        authorizationVerification(userId);
 
         return eventService.getEventRequestsForUser(userId, eventId);
     }
 
-    public void authorizationVerification(Long userId) {
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        Object principal = authentication.getPrincipal();
-
-        if (!(principal instanceof User)) {
-            throw new ForbiddenException("Пользователь не авторизован корректно");
-        }
-
-        User currentUser = (User) principal;
-        Long currentUserId = currentUser.getId();
-
-        if (!userId.equals(currentUserId)) {
-            throw new ForbiddenException("Нельзя создавать события от чужого имени авторизованы как пользователь:"
-                    + currentUserId + " но пытаетесь действовать от имени: " + userId
-            );
-        }
-    }
+//    public void authorizationVerification(Long userId) {
+//        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+//        Object principal = authentication.getPrincipal();
+//
+//        if (!(principal instanceof User)) {
+//            throw new ForbiddenException("Пользователь не авторизован корректно");
+//        }
+//
+//        User currentUser = (User) principal;
+//        Long currentUserId = currentUser.getId();
+//
+//        if (!userId.equals(currentUserId)) {
+//            throw new ForbiddenException("Нельзя создавать события от чужого имени авторизованы как пользователь:"
+//                    + currentUserId + " но пытаетесь действовать от имени: " + userId
+//            );
+//        }
+//    }
 
 
     @GetMapping("/events")
--- main_svc/src/main/java/ru/practicum/main/request/RequestController.java
+++ main_svc/src/main/java/ru/practicum/main/request/RequestController.java
@@ -3,13 +3,9 @@ package ru.practicum.main.request;
 
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
-import ru.practicum.main.exception.ForbiddenException;
 import ru.practicum.main.request.dto.ParticipationRequestDto;
 import ru.practicum.main.request.service.RequestService;
-import ru.practicum.main.user.model.User;
 
 import java.util.List;
 
@@ -24,7 +20,7 @@ public class RequestController {
     @PostMapping("/users/{userId}/requests")
     public ParticipationRequestDto createRequest(@PathVariable Long userId, @PathVariable Long eventId) {
 
-        authorizationVerification(userId);
+//        authorizationVerification(userId);
 
         return requestService.createRequest(userId, eventId);
     }
@@ -32,7 +28,7 @@ public class RequestController {
     @GetMapping("/users/{userId}/requests")
     public List<ParticipationRequestDto> getRequest(@PathVariable Long userId) {
 
-        authorizationVerification(userId);
+//        authorizationVerification(userId);
 
         return requestService.getRequest(userId);
     }
@@ -40,28 +36,28 @@ public class RequestController {
     @PatchMapping("/users/{userId}/requests/{requestId}/cancel")
     public ParticipationRequestDto cancelRequest(@PathVariable Long userId, @RequestParam Long requestId) {
 
-        authorizationVerification(userId);
+//        authorizationVerification(userId);
 
         return requestService.cancelRequest(userId, requestId);
     }
 
 
-    public void authorizationVerification(Long userId) {
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        Object principal = authentication.getPrincipal();
-
-        if (!(principal instanceof User)) {
-            throw new ForbiddenException("Пользователь не авторизован корректно");
-        }
-
-        User currentUser = (User) principal;
-        Long currentUserId = currentUser.getId();
-
-        if (!userId.equals(currentUserId)) {
-            throw new ForbiddenException("Нельзя создавать события от чужого имени авторизованы как пользователь:"
-                    + currentUserId + " но пытаетесь действовать от имени: " + userId
-            );
-        }
-    }
+//    public void authorizationVerification(Long userId) {
+//        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+//        Object principal = authentication.getPrincipal();
+//
+//        if (!(principal instanceof User)) {
+//            throw new ForbiddenException("Пользователь не авторизован корректно");
+//        }
+//
+//        User currentUser = (User) principal;
+//        Long currentUserId = currentUser.getId();
+//
+//        if (!userId.equals(currentUserId)) {
+//            throw new ForbiddenException("Нельзя создавать события от чужого имени авторизованы как пользователь:"
+//                    + currentUserId + " но пытаетесь действовать от имени: " + userId
+//            );
+//        }
+//    }
 
 }
